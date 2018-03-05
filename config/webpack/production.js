bf682173daf1df66415e4cb0e920757e37e6caa5
Trey Evans <lewis.r.evans@gmail.com>
2018-03-05 16:34:51 -0500

    Fixing compilation.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,10 @@
 const environment = require('./environment')
+const typescript =  require('./loaders/aot_typescript')
+const angular_html =  require('./loaders/angular_html')
+const angular_css =  require('./loaders/angular_css')
+
+environment.loaders.append('aot_typescript', typescript)
+environment.loaders.append('angular_html', angular_html)
+environment.loaders.append('angular_css', angular_css)
 
 module.exports = environment.toWebpackConfig()
